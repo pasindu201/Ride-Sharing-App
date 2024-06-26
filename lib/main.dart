024eebfc2f0d2c4e38a50942e0575b9017ca14c0
pasindu201 <vikkramanayakaagps.20@uom.lk>
2024-06-26 07:36:28 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:users/screens/register_screen.dart';
+import 'package:users/theme_provider/theme_provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      themeMode: ThemeMode.system,
+      theme: MyThemes.lightTheme,
+      darkTheme: MyThemes.darkTheme,
+      home: RegisterScreen(),
+    );
+  }
+}
+
+
+
